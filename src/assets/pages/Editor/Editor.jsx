47e42e7d35aa14dfe966177b46e3fe42188f5b47
prabh-ux <prabhkiratsinghbusiness@gmail.com>
2025-07-28 21:20:11 +0530

    added render direction on movable

--- src/assets/pages/Editor/Editor.jsx
+++ src/assets/pages/Editor/Editor.jsx
@@ -345,8 +345,7 @@ const Editor = () => {
                           rotatable
                           throttleDrag={0}
                           renderDirections={["nw", "ne", "sw", "se", "n", "s", "e", "w"]}
- minWidth={30}
-  minHeight={30}
+
                           onDrag={({ target, left, top }) => {
                             target.style.left = `${left}px`;
                             target.style.top = `${top}px`;
